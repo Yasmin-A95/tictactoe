f48f7d3a91ab0b898034a896b757f4a42968420c
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-07-06 16:38:13 +1000

    click function

--- js/dom.js
+++ js/dom.js
@@ -1,17 +1,41 @@
 //this knows NOTHING about rules it simply connects the DOM to the rules in scripts.js
 
-//lots of event handlers and all the dom manipulation 
+//lots of event handlers and all the dom manipulation
 
 //deploy every significant change
-const render = function () {
-    //this handy render function will update every single part of  DOM with the latest values from the board 
+// const render = function () {
+//this handy render function will update every single part of  DOM with the latest values from the board
 
-    // in render I'll use jQuery to grab parts of the dom ${likethis}
-    // and update the display like this $
-  $(document).ready(function () {
-    //
-    render();
+// in render I'll use jQuery to grab parts of the dom ${likethis}
+// and update the display like this $("things")
+// $(document).ready(function () {
+  //
+  // render();
+  // the things
+  const tiles = $(".tiles");
+  const Player_X = "X";
+  const Player_O = "O";
+  let turn = Player_X;
 
-    //i may need to call render in every single event handler in here
-  });
-};
+  let boardState = Array(tiles.length);
+  boardState.fill(null);
+  // could also do like this
+  // let boardState = [*,*,*,*,*,*,*,*] since I know how many tiles there are..
+
+  const gameOverArea = $("#game-over-area");
+
+  const gameOverText = $("#game-over-text");
+
+  const playAgainButton = $("#play-again");
+
+  const clickFunction = function () {
+    alert("hi");
+  };
+
+  tiles.on("click", clickFunction);
+
+  // I may need to call render in every single event handler in here
+
+// });
+
+// };
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //this code knows NOTHING about the DOM
 
-//just rules and logic for "gameplay" 
-// no css no anything visual, that doesn't exist. the game IS the rules 
+//just rules and logic for "gameplay"
+// no css no anything visual, that doesn't exist. the game IS the rules
 
 //this is the single source of truth about who's turn it is and what 'tiles' belong to who
 
 //deploy every significant change
+
