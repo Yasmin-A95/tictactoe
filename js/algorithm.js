09c6579ec06331d3bbfd0d68bef97e92bf88f130
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-07-06 20:57:19 +1000

    start of a pseudocode algorithm

---
+++ js/algorithm.js
@@ -0,0 +1,13 @@
+let num1 = Math.floor(Math.random() * wins.length);
+let thisGuy = wins[num1];
+Math.floor(Math.random() * thisGuy.length);
+
+// num1 is one of the arrays in wins's indicies number
+// so its like wins[1] .. or whatever other one
+// then thisGuy is wins[1][2] .. or something. Yeah?
+// if boardState[wins[1][2]] "*" {
+// do stuff
+// } else {
+//         try again;
+//}
+//
