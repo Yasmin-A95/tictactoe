21b5db2d9279089182cd85f032014a1bc25de516
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-07-08 10:24:44 +1000

    cleaned up version, removed dead code and comments

--- js/dom.js
+++ js/dom.js
@@ -1,22 +1,8 @@
 //this knows NOTHING about rules it simply connects the DOM to the rules in scripts.js
 
-//lots of event handlers and all the dom manipulation
-
-//deploy every significant change
-// const render = function () {
-//this handy render function will update every single part of  DOM with the latest values from the board
-
-// in render I'll use jQuery to grab parts of the dom ${likethis}
-// and update the display like this $("things")
-// $(document).ready(function () {
-//
-// render();
-// the things
 const tiles = $(".tiles");
 const button = $("button");
 let turn = Player_X;
-// could also do like this
-// let boardState = [*,*,*,*,*,*,*,*] since I know how many tiles there are..
 
 const gameOverArea = $("#game-over-area");
 
@@ -51,11 +37,6 @@ const clickFunction = function (event) {
     turn = Player_O;
     oRandomMove();
   }
-  //  else {
-  //   tile.innerText = Player_O;
-  //   boardState[tileNumber] = Player_O;
-  //   turn = Player_X;
-  // }
   checkWinner();
 };
 
@@ -71,8 +52,4 @@ button.on("click", function () {
   gameOverArea.addClass("hidden");
 });
 
-// I may need to call render in every single event handler in here
 
-// });
-
-// };
